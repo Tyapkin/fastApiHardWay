61ee06f951256da206a1dc3feefcc976d0984851
Alexander Tyapkin <atyapkin20@gmail.com>
2022-12-17 21:16:12 +0300

    static

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from backend.core.config import settings
 from backend.apis.general_pages.route_homepage import general_page_router
 
@@ -7,9 +8,14 @@ def include_router(_app):
     _app.include_router(general_page_router)
 
 
+def configure_static(_app):
+    _app.mount('/static', StaticFiles(directory='backend/static'), name='static')
+
+
 def start_application():
     _app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(_app)
+    configure_static(_app)
     return _app
 
 
